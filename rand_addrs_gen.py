a192c3503dede6f2b66fc3146e586af78d4d3926
Mason Biamonte <masonbiamonte@gmail.com>
2021-01-17 12:14:03 -0600

    add rand_addrs_gen.py

---
+++ rand_addrs_gen.py
@@ -0,0 +1,16 @@
+import glob
+import googlemaps
+import numpy as np
+import os
+import pandas as pd
+import sys
+
+from utils import generate_random_addrs
+
+hq_file = 'rothko_hq.csv'
+coords_file = 'random_addresses.csv'
+
+num_addrs = int(input(f'Number of non-HQ random addresses: '))+1
+generate_random_addrs(num_addrs, hq_file, coords_file) # does not use API calls
+
+
